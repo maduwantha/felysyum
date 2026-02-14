72ecdd43e398474570b627b1e26574176e53cce5
Maduwantha <maduwantha.amarathunga@gmail.com>
2026-02-14 21:59:51 +0900

    s
    s

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
   turbopack: {
     resolveAlias: {
-      "@": "./src",
-      "@public": "./public",
+      '@': './src',
+      '@public': './public',
     },
   },
   images: {
     unoptimized: true,
     qualities: [25, 50, 75, 100],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "connect-src 'self' https://www.google-analytics.com https://gasstation.polygon.technology https://polygon-rpc.com",
-          },
-        ],
-      },
-    ];
   },
 };
 
