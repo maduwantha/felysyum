8857d60f84c69c7724289fcef125260069233339
Maduwantha <maduwantha.amarathunga@gmail.com>
2026-02-14 21:16:51 +0900

    d
    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import SmoothScrollProvider from '@/components/shared/SmoothScroll';
-import { ThemeProvider } from '@/components/shared/ThemeProvider';
-import DemoShowcase from '@/components/shared/demo-showcase';
-import Footer from '@/components/shared/footer/Footer';
-import Navbar from '@/components/shared/navbar/Navbar';
-import { interTight } from '@/utils/font';
-import { generateMetadata } from '@/utils/generateMetaData';
-import { Metadata } from 'next';
-import { ReactNode, Suspense } from 'react';
-import './globals.css';
+import SmoothScrollProvider from "@/components/shared/SmoothScroll";
+import { ThemeProvider } from "@/components/shared/ThemeProvider";
+import DemoShowcase from "@/components/shared/demo-showcase";
+import Footer from "@/components/shared/footer/Footer";
+import Navbar from "@/components/shared/navbar/Navbar";
+import { interTight } from "@/utils/font";
+import { generateMetadata } from "@/utils/generateMetaData";
+import { Metadata } from "next";
+import { ReactNode, Suspense } from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Felysyum : Elysium's Digital Gold",
   description: "Felysyum is Elysium's Digital Gold.",
   icons: {
-    icon: '/favicon.png',
+    icon: "/favicon.png",
   },
 };
 
@@ -24,8 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="connect-src 'self' https://www.google-analytics.com https://gasstation.polygon.technology https://polygon-rpc.com"
+        />
+      </head>
       <body className={`${interTight.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Suspense>
             <SmoothScrollProvider>
               <Navbar />
