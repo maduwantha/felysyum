6e3f4c04e01b4f39bd3c8f996d1eb110d19eeb14
Maduwantha <maduwantha.amarathunga@gmail.com>
2026-03-14 23:33:24 +0900

    htaccess
    htaccess

--- src/utils/generateMetaData.ts
+++ src/utils/generateMetaData.ts
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 export const DEFAULT_URL = "https://dv.felysyum.com";
+
 export const DEFAULT_TITLE = "Felysyum : Elysium's Digital Gold";
 export const DEFAULT_DESCRIPTION =
   "Discover your digital elysium at The Oasis of Legends, a vibrant ecosystem fueled by Felysyum (FELY) cryptocurrency.";
